50ff5b09f177f826d18a3d86235ca185305949d3
Jeremy Kenyon <lanthos@gmail.com>
2016-01-24 01:11:31 -0800

    mouse placeholder and basic movement
    Movement is backwards right now but I’m too tired to figure out what’s
    going wrong.  Will work on making it correct in the morning.

---
+++ assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+	public float cameraUpMod = 25.0f;
+	public float cameraRightMod = -30.0f;
+	public float cameraForwardMod = 15.0f;
+	
+	Vector3 moveCamTo;
+	// Use this for initialization
+	void Start () {
+		moveCamTo = transform.position - transform.forward * cameraForwardMod + Vector3.up * cameraUpMod;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveCamTo = transform.position - transform.forward * cameraForwardMod + Vector3.up * cameraUpMod;
+		Camera.main.transform.LookAt (transform.position + transform.forward * cameraForwardMod);
+
+		
+		float bias = 0.92f;
+		Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+
+	}
+}
---
+++ assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+
+
+	void Movement(){
+		transform.position = transform.position + Vector3.forward * (Time.deltaTime * movementSpeed * Input.GetAxis ("Vertical"));
+		transform.position = transform.position + Vector3.right * (Time.deltaTime * movementSpeed * Input.GetAxis ("Horizontal"));
+	}
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+}
