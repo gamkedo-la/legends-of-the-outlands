820d24e28ec23e2ff898d0c8456d57563c53dc08
TimWaskett <tim.waskett@gmail.com>
2016-02-15 13:34:20 +0000

    Added Scales prefab
    Scales prefab has animator and test scrip attached and can be tried out
    in the test scene.

---
+++ assets/Anim/ScalesTestAnimatorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ScalesTestAnimatorScript : MonoBehaviour
+{
+    private Animator m_anim;
+
+
+	void Start()
+    {
+        m_anim = GetComponent<Animator>();
+	}
+
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            print("Plate 1 down");
+            m_anim.SetTrigger("Plate 1 down");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            m_anim.SetTrigger("Plate 2 down");
+
+        if (Input.GetKeyDown(KeyCode.L))
+            m_anim.SetTrigger("Level");
+    }
+}
