40ae249ef66ee12e0f19b523d45da0f3c4ad008c
Chris DeLeon <gamkedo@gmail.com>
2016-02-17 01:32:34 -0800

    Paper towel release drops roll to form bridge over syrup

---
+++ assets/Scripts/TriggerPlayAnimOnce.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPlayAnimOnce : MonoBehaviour {
+	public GameObject playAnimOn;
+
+	bool playedYet = false;
+
+	void OnTriggerEnter (Collider whoEnters) {
+		if(playedYet == false && whoEnters.tag == "Player") {
+			playedYet = true;
+			Animator playWhichAnim = playAnimOn.GetComponent<Animator>();
+			playWhichAnim.enabled = true;
+		}
+	}
+}
