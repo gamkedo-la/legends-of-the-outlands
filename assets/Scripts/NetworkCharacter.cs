636d84517c0b2ded0822dc69a975a7cbdd4a83b2
Jeremy Kenyon <lanthos@gmail.com>
2016-02-17 23:12:04 -0800

    Removed old single player code

--- assets/Scripts/NetworkCharacter.cs
+++ assets/Scripts/NetworkCharacter.cs
@@ -3,28 +3,21 @@ using Photon;
 
 public class NetworkCharacter : Photon.MonoBehaviour {
 
-	RandomMatchmaker matchSettings;
 	Vector3 correctPlayerPos;
 	Quaternion correctPlayerRot;
 
-	void Start(){
-		matchSettings = GameObject.Find("ScriptsManager").GetComponent<RandomMatchmaker> ();
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (matchSettings.singlePlayer == false) {
 			if (photonView.isMine == false)
 			{
 				transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
 				transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
 			}
-		}
 	}
 
 	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 	{
-		if (matchSettings.singlePlayer == false) {
 			if (stream.isWriting)
 			{
 				// We own this player: send the others our data
@@ -38,6 +31,5 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 				this.correctPlayerPos = (Vector3)stream.ReceiveNext();
 				this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
 			}
-		}
 	}
 }
