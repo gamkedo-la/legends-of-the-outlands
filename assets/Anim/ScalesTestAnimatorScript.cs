99fe028440f6dc534a41718b6572aee7f067ae91
Nicholas Polchies <npolchies@yahoo.com>
2016-02-18 02:36:30 -0400

    Added weight sensitivity to scale
    Weight sensitivity is based on rigidbody mass of objects in contact with
    the scale plates.

--- assets/Anim/ScalesTestAnimatorScript.cs
+++ assets/Anim/ScalesTestAnimatorScript.cs
@@ -7,33 +7,47 @@ public class ScalesTestAnimatorScript : MonoBehaviour
     private Animator m_anim;
     public Collider plate1Container, plate2Container;
     public float weight1, weight2;
+    public ScaleState currentState = ScaleState.Level;
+
+    public enum ScaleState{Plate1Down, Level, Plate2Down};
 
 	void Start()
     {
         m_anim = GetComponent<Animator>();
-	}
+    }
 
 
 	void Update()
     {
-        /*
-        if (weight1 > weight2 + 0.1f)
+        if (weight1 > weight2 + 0.1f && currentState != ScaleState.Plate1Down)
         {
             print("Plate 1 down");
             m_anim.SetTrigger("Plate 1 down");
+            currentState = ScaleState.Plate1Down;
         }
 
-        if (weight2 > weight1 + 0.1f)
+        if (weight2 > weight1 + 0.1f && currentState != ScaleState.Plate2Down)
         {
             print("Plate 2 down");
             m_anim.SetTrigger("Plate 2 down");
+            currentState = ScaleState.Plate2Down;
         }
 
-        if (weight1 >= weight2 - 0.1f && weight2 >= weight1 - 0.1f)
+        if (weight1 >= weight2 - 0.1f && weight2 >= weight1 - 0.1f && currentState != ScaleState.Level)
         {
             print("Plates level");
             m_anim.SetTrigger("Level");
+            currentState = ScaleState.Level;
         }
-        */
+    }
+
+    public void changeWeight(bool isPlate1, float weight)
+    {
+        if(isPlate1){
+            weight1 += weight;
+        }
+        else{
+            weight2 += weight;
+        }
     }
 }
---
+++ assets/ScalesPlateWeightCounter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalesPlateWeightCounter : MonoBehaviour {
+    ScalesTestAnimatorScript scales;
+    public bool isPlate1;
+
+    void Start()
+    {
+        scales = transform.parent.GetComponent<ScalesTestAnimatorScript>();
+    }
+
+    void OnCollisionEnter(Collision collision){
+        scales.changeWeight(isPlate1, collision.rigidbody.mass);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        scales.changeWeight(isPlate1, -collision.rigidbody.mass);
+    }
+}
