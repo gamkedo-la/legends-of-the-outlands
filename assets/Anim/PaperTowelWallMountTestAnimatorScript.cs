b0ef84b7a945d21e49bb3c084f97387d006c9783
TimWaskett <tim.waskett@gmail.com>
2016-02-19 10:11:02 +0000

    Added paper towel wall mount prefab with animations
    Also added a test scene and test animator script.

---
+++ assets/Anim/PaperTowelWallMountTestAnimatorScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class PaperTowelWallMountTestAnimatorScript : MonoBehaviour
+{
+    private Animator m_anim;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.T))
+        {
+            m_anim.SetTrigger("Open wall mount");
+            print("Release paper towel holder");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            m_anim.SetTrigger("Reset wall mount");
+            print("Reset paper towel holder");
+        }
+    }
+}
