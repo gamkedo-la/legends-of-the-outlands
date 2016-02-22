da712869ad3245a76bbe3b4ff58dc8c7d9336b9a
TimWaskett <tim.waskett@gmail.com>
2016-02-22 13:33:29 +0000

    Added animation to the right window for opening/closing, plus made a prefab of the kitchen model

---
+++ assets/Anim/WindowTestAnimatorScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class WindowTestAnimatorScript : MonoBehaviour
+{
+    private Animator m_anim;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            m_anim.SetTrigger("Open window");
+            print("Open window");
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            m_anim.SetTrigger("Close window");
+            print("Close window");
+        }
+    }
+}
