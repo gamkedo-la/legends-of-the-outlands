ada454170349acd8f67dff8d6c92241a7b68bcba
Chris DeLeon <gamkedo@gmail.com>
2016-02-24 23:57:38 -0800

    bowl orientation and collisions fixed, song transition added (crude/rough version for now), spawn point moved into tunnel pipes. Still using main scene, next is ensuring puzzles can be executed well enough to get through whole level

--- assets/Scripts/SongSwitcher.cs
+++ assets/Scripts/SongSwitcher.cs
@@ -8,27 +8,61 @@ public class SongSwitcher : MonoBehaviour {
 
 	enum SongMode {TUNNEL,TRANSITON, KITCHEN, NONE_YET};
 	SongMode songNow = SongMode.NONE_YET;
+	SongMode songNext = SongMode.NONE_YET;
 
 	AudioSource jukeBox;
+	AudioSource jukeBoxFader;
+	bool blockNewEnum = false;
 
 	public Transform groundPlaneForY;
 
 	// Use this for initialization
 	void Start () {
 		jukeBox = GetComponent<AudioSource>();
+		jukeBoxFader = transform.GetChild(0).GetComponent<AudioSource>();
+
+		songNow = SongMode.TUNNEL;
+		jukeBox.clip = tunnelSong;
+		jukeBox.Play();
+
+		// jukeBoxFader.clip = transitionSong;
+		// jukeBoxFader.Play();
+	}
+
+	IEnumerator SongTransition() {
+		blockNewEnum = true;
+		jukeBox.clip = transitionSong;
+		jukeBox.Play();
+		yield return new WaitForSeconds(transitionSong.length);
+		Debug.Log("MUSIC TRANSITION DONE");
+		switch( songNow ) {
+		case SongMode.TUNNEL:
+			jukeBox.clip = tunnelSong;
+			jukeBox.Play();
+			break;
+		case SongMode.KITCHEN:
+			jukeBox.clip = kitchenSong;
+			jukeBox.Play();
+			break;
+		default:
+			Debug.Log("Song transition is only meant to feed into kitchen or tunnel song");
+			break;
+		}
+		blockNewEnum = false;
 	}
 
 	void CheckAndPlay(SongMode songEnum, AudioClip songFile) {
 		if(songNow != songEnum) {
 			songNow = songEnum;
-			jukeBox.clip = songFile;
-			jukeBox.Play();
+			if(blockNewEnum == false) {
+				StartCoroutine(SongTransition());
+			}
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.y < groundPlaneForY.position.y) {
+		if(transform.position.y > groundPlaneForY.position.y) {
 			CheckAndPlay(SongMode.KITCHEN, kitchenSong);
 		} else {
 			CheckAndPlay(SongMode.TUNNEL, tunnelSong);
