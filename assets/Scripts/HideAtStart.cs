32bb7445dbc4dfddbaff2718e5667cd703a52164
Chris DeLeon <gamkedo@gmail.com>
2016-02-25 03:20:35 -0800

    Initial text for credits (not yet specific) and help text

---
+++ assets/Scripts/HideAtStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideAtStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive(false);
+	}
+}
