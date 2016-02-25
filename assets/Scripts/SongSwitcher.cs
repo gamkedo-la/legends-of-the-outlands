995738ef171c425e7ec8ff11e9e89c7dc8894247
Chris DeLeon <gamkedo@gmail.com>
2016-02-24 23:32:48 -0800

    Music added (so far lacking transition, but switches if in/out of crawl space). Ramp added to pipe in crawl space so easier to enter. Rat prefab colliders replaced with capsules to better handle tiny steps without getting caught on them.

---
+++ assets/Scripts/SongSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongSwitcher : MonoBehaviour {
+	public AudioClip tunnelSong;
+	public AudioClip transitionSong;
+	public AudioClip kitchenSong;
+
+	enum SongMode {TUNNEL,TRANSITON, KITCHEN, NONE_YET};
+	SongMode songNow = SongMode.NONE_YET;
+
+	AudioSource jukeBox;
+
+	public Transform groundPlaneForY;
+
+	// Use this for initialization
+	void Start () {
+		jukeBox = GetComponent<AudioSource>();
+	}
+
+	void CheckAndPlay(SongMode songEnum, AudioClip songFile) {
+		if(songNow != songEnum) {
+			songNow = songEnum;
+			jukeBox.clip = songFile;
+			jukeBox.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < groundPlaneForY.position.y) {
+			CheckAndPlay(SongMode.KITCHEN, kitchenSong);
+		} else {
+			CheckAndPlay(SongMode.TUNNEL, tunnelSong);
+		}
+	}
+}
