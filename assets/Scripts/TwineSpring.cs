1de972183185129dc4be0923de017db186b3fd74
Chris DeLeon <gamkedo@gmail.com>
2016-03-03 18:53:35 -0800

    Twine spring gag partially set up, gotta run to meeting for now though :)

---
+++ assets/Scripts/TwineSpring.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwineSpring : Photon.MonoBehaviour {
+
+	public string removeThisNameOnEntry;
+	public Animation[] animOnEntry;
+	bool bandAttachedYet = false;
+
+	void Start () {
+		for(int i = 0; i < animOnEntry.Length; i++) {
+			animOnEntry[i].enabled = false;
+		}
+	}
+
+	[PunRPC]
+	void AttachPin() {
+		if(bandAttachedYet) {
+			return;
+		}
+		bandAttachedYet = true;
+		GameObject pinUsed = GameObject.Find(removeThisNameOnEntry);
+		if(pinUsed) {
+			pinUsed.SetActive(false);
+		}
+		for(int i = 0; i < animOnEntry.Length; i++) {
+			animOnEntry[i].enabled = true;
+		}
+
+		this.enabled = false;
+	}
+	
+	void OnTriggerEnter (Collider entered) {
+		Debug.Log(entered.name);
+		if(entered.name.Contains(removeThisNameOnEntry)) {
+			removeThisNameOnEntry = entered.name; // exact match for removal soon
+			if(photonView == null) {
+				AttachPin();
+			} else {
+				photonView.RPC("AttachPin", PhotonTargets.All);
+			}
+		}
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(bandAttachedYet);
+		}
+		else
+		{
+			// Network player, receive data
+			bandAttachedYet = (bool) stream.ReceiveNext();
+		}
+	}
+}
